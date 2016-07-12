4e3834ca0db2b6b0cd91eb3563783e11c60d407f
Reed Branson <reed.branson@gmail.com>
2016-07-12 16:34:51 -0400

    demoing the persistent user

--- server/api/users/user.router.js
+++ server/api/users/user.router.js
@@ -56,4 +56,19 @@ router.delete('/:id', function (req, res, next) {
   .catch(next);
 });
 
+router.post('/login', function (req, res, next) {
+  User.findOne({
+    where: req.body
+  })
+  .then(function (user) {
+    if (!user) {
+      res.sendStatus(401);
+    } else {
+    req.session.userId = user.id;
+    res.sendStatus(204);
+    }
+  })
+  .catch(next);
+});
+
 module.exports = router;
--- server/app/index.js
+++ server/app/index.js
@@ -1,7 +1,17 @@
-'use strict'; 
+'use strict';
 
 var app = require('express')();
 var path = require('path');
+var session = require('express-session');
+
+app.use(session({
+  secret: 'reediskewl'
+}))
+
+app.use(function (req, res, next) {
+  console.log('session', req.session);
+  next();
+});
 
 app.use(require('./logging.middleware'));
 
@@ -9,7 +19,14 @@ app.use(require('./request-state.middleware'));
 
 app.use(require('./statics.middleware'));
 
+// app.use('/api', function (req, res, next) {
+//   if (!req.session.counter) req.session.counter = 0;
+//   console.log('counter', ++req.session.counter);
+//   next();
+// });
+
 app.use('/api', require('../api/api.router'));
+
 
 var validFrontendRoutes = ['/', '/stories', '/users', '/stories/:id', '/users/:id', '/signup', '/login'];
 var indexPath = path.join(__dirname, '..', '..', 'public', 'index.html');
