7b97078c63d33c1ceae01097d4b24f064a0d7ff9
Reed Branson <reed.branson@gmail.com>
2016-07-13 11:13:46 -0400

    logout working, but not yet fully implemented

--- browser/components/navbar/navbar.directive.js
+++ browser/components/navbar/navbar.directive.js
@@ -5,6 +5,7 @@ app.directive('navbar', function ($state, $location, LoginFactory) {
     restrict: 'E',
     templateUrl: '/browser/components/navbar/navbar.html',
     link: function (scope) {
+      scope.logoutFunc = LoginFactory.logoutFunc
       scope.pathStartsWithStatePath = function (state) {
         var partial = $state.href(state);
         var path = $location.path();
